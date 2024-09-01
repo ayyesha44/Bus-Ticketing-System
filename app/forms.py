b00bae955d6a676e3cb2ac84c58b698f09ef3ad1
ayesha <ashhaazmar@gmail.com>
2024-09-01 21:28:52 +0800

    create registration form class

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
+
 
 class Loginform(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     rememberme = BooleanField('Remember me')
     submit = SubmitField('Log in')
+
+class Registrationform(FlaskForm):
+    email = StringField('Email address', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    retypepass = PasswordField('Retype password', validators=[DataRequired(), EqualTo('password')])
+    signup = SubmitField('Sign up')
